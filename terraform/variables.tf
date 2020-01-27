debad6624eec7f2e9bd010b84b7056df16d77ee0
Patrick LaRoche <patrick.jr.laroche@gmail.com>
2020-01-27 16:06:58 -0400

    Making a bit more generic

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "nginx:latest"
 }
 
 variable "app_port" {
